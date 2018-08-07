1bbae85311d9b2da83cbf1925c4699d00611954c
Daniella LIberman <daliberman@itba.edu.ar>
2018-08-06 23:28:52 -0300

    Finished algorithm implementation

--- ss_tp1/src/ss_tp1/CellMethodIndex.java
+++ ss_tp1/src/ss_tp1/CellMethodIndex.java
@@ -12,46 +12,56 @@ import java.util.function.Predicate;
 public class CellMethodIndex {
 	public static void main(String[] args) {
 		int n = 7;
-		int l = 10;
-		int rc = 1;
-		double m = 2;
-		int size = (int)Math.floor(l/m);
-		List<Particle> particles = new ArrayList<>(); 
+		double l = 15; /** longitud de un lado */
+		double rc = 15; /** radio de busqueda de vecinos */
+		int m = 1; /** celdas por lado */
+		Set<Particle> particles = new HashSet<>(); 
 		
-		Map<Cell, List<Particle>> cells = new HashMap<>();
+		Map<Cell, Set<Particle>> cells = new HashMap<>();
 		
 		/**TODO: read from file*/
-		particles.add(new Particle(0.1,0.1,0.1));
-		particles.add(new Particle(3,4,0.1));
-		particles.add(new Particle(4,2,0.1));
-		particles.add(new Particle(6,1,0.1));
-		particles.add(new Particle(5,5,0.1));
-		particles.add(new Particle(1,4,0.1));
-		particles.add(new Particle(2,2,0.1));
+		particles.add(new Particle(4.5,2.7,0.1));
+		particles.add(new Particle(14.1,3.6,0.1));
+		particles.add(new Particle(12.6,5.4,0.1));
+		particles.add(new Particle(4.5,8.7,0.1));
+		particles.add(new Particle(1.2,10.8,0.1));
+		particles.add(new Particle(7.5,14.4,0.1));
 		
 		/** add each particle at corresponding cell*/
 		for(Particle p : particles) {
-			Cell cell = p.getCell(m);
+			Cell cell = p.getCell(m, l);
 			
 			if(!cells.containsKey(cell)) {
-				cells.put(cell, new ArrayList<>());
+				cells.put(cell, new HashSet<>());
 			}
 			cells.get(cell).add(p);
 		}
 		
 		for(Particle particle : particles) {
-			Cell particleCell = particle.getCell(m);
-			List<Particle> neighbours = new ArrayList<>();
-			neighbours.addAll(cells.get(particle.getCell(m)));
+			Cell particleCell = particle.getCell(m, l);
+			particle.getNeighbours().addAll(cells.get(particle.getCell(m, l)));
 
-			for(Cell c : particleCell.getNeighbours(size)) {
-				neighbours.addAll(cells.get(c));
+			for(Cell c : particleCell.getNeighbours(m)) {
+				Set<Particle> neighbours = cells.get(c);
+				if(neighbours != null)
+					particle.getNeighbours().addAll(neighbours);
 			}
 			/** neighbours now contains all particles that need to be compared */			
-			
-			/** TODO: p.distanceTo(cada particula de neighbour. si no cumple elimino)*/
-			neighbours.removeIf(p -> {return p.getR() > 0;});
+			particle.getNeighbours().removeIf(p -> p.distanceTo(particle) > rc);
+			particle.getNeighbours().remove(particle);
+			for(Particle neighbour : particle.getNeighbours()) {
+				neighbour.getNeighbours().add(particle);
+			}
 		}
-		
+		printParticles(particles);
+	}
+	
+	public static void printParticles(Set<Particle> ps) {
+		for (Particle p : ps) {
+			System.out.println(p + " Neighbours:");
+			for (Particle neighbour : p.getNeighbours()) {
+				System.out.println(" -" + neighbour);
+			}
+		}
 	}
 }
--- ss_tp1/src/ss_tp1/Particle.java
+++ ss_tp1/src/ss_tp1/Particle.java
@@ -1,14 +1,24 @@
 package ss_tp1;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Particle {
+	static private long nextId = 0;
+	private long id;
 	private double x;
 	private double y;
 	private double r;
+	private Set<Particle> neighbours;
 	public Particle(double x, double y, double r) {
 		super();
 		this.x = x;
 		this.y = y;
 		this.r = r;
+		this.id = nextId++;
+		this.neighbours = new HashSet<>();
 	}
 	public double getX() {
 		return x;
@@ -19,10 +29,38 @@ public class Particle {
 	public double getR() {
 		return r;
 	}
-	public Cell getCell(double m) {
-		return new Cell((int)Math.floor(y/m), (int)Math.floor(x/m));
+	public Set<Particle> getNeighbours() {
+		return neighbours;
+	}
+	public Cell getCell(int m, double l) {
+		return new Cell((int)Math.floor(y*m/l), (int)Math.floor(x*m/l));
 	}
 	public double distanceTo(Particle particle) {
 		return Math.sqrt((x-particle.x)*(x-particle.x)+(y-particle.y)*(y-particle.y)) - r - particle.r;
 	}
+	@Override
+	public String toString() {
+		return "Particle (" + x + ", " + y + ", " + r + ")";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Particle other = (Particle) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
 }
